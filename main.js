5e0f49362a88b462c7561e37963cf4974d477305
Mohamed Elsayed <75834817+mo550@users.noreply.github.com>
2022-02-22 11:13:00 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-/* Main Functionality */
+/* Main Variables */
 const gameBlock = document.querySelectorAll('.game-block');
 const revealBtn = document.getElementById('reveal-btn');
 
-// Flip Card
+// Flip Card When Clicking
 gameBlock.forEach((block) => {
   block.addEventListener('click', () => {
     block.classList.add('is-flipped');
   });
 });
 
-// Reveal All Cards
+// Reveal All Cards When Clecking On "Reveal All"
 revealBtn.addEventListener('click', () => {
   gameBlock.forEach((block) => {
     block.classList.add('is-flipped');
